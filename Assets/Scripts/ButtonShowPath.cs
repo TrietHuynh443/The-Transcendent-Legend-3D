876d32afb92029a9441533d2c658545f00bcdc37
EricNguyen1203 <21120132@student.hcmus.edu.vn>
2025-01-21 18:27:35 +0700

    feat: get path invisible in Challenge 3

---
+++ Assets/Scripts/ButtonShowPath.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using ExitGames.Client.Photon.StructWrapping;
+using UnityEngine;
+
+public class ButtonShowPath : MonoBehaviour
+{
+    public GameObject path;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter()
+    {
+        path.SetActive(true);
+        
+        // Set transform of position y lower than 0.5
+        transform.position = new Vector3(transform.position.x, transform.position.y - 0.1f, transform.position.z);
+    }
+
+    void OnTriggerExit()
+    {
+        path.SetActive(false);
+        transform.position = new Vector3(transform.position.x, transform.position.y + 0.1f, transform.position.z);
+    }
+}
