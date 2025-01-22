1c300ca7ef0f5cb63e0ad37e976e38e6ba83d933
huynhlonguni <106131819+huynhlonguni@users.noreply.github.com>
2025-01-22 13:39:49 +0700

    fix: exit trigger

--- Assets/Scenes/Finish/Exit.cs
+++ Assets/Scenes/Finish/Exit.cs
@@ -1,24 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
 public class Exit : MonoBehaviour
 {
     public Canvas canvas;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private List<int> triggerList = new List<int>();
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter(Collider other)
     {
+        int trigger = other.gameObject.GetComponent<PhotonView>().Owner.ActorNumber;
+        if (!triggerList.Contains(trigger))
+            triggerList.Add(trigger);
         
-    }
-
-    void OnTriggerEnter()
-    {
+        for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
+        {
+            int player = PhotonNetwork.PlayerList[i].ActorNumber;
+            if (!triggerList.Contains(player))
+                return;
+        }
+        
         AudioManager.Instance.musicSource.Stop();
         AudioManager.Instance.PlayMusic("Win");
         canvas.gameObject.SetActive(true);
